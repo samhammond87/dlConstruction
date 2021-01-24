eca99ae5fdf30778cf1c32d097204e1c7a315f42
samhammond87 <sam.hammond1987@gmail.com>
2021-01-25 10:46:00 +1100

    routing added

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,10 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  scope '/api' do
+    get '/timesheets', to: 'timesheets#index'
+    post '/timesheets', to: 'timesheets#create'
+    get '/timesheets/:id', to: 'timesheet#show'
+    put '/timesheets/:id', to: 'timesheet#update'
+    delete '/timesheets/:id', to: 'timesheet#destroy'
+  end
 end
+
