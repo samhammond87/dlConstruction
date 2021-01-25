cd6cbfe3582edb684ea1b7b7ed4eed52ab4434fc
samhammond87 <sam.hammond1987@gmail.com>
2021-01-25 16:23:47 +1100

    created user model with validation

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_01_24_233148) do
+ActiveRecord::Schema.define(version: 2021_01_25_051804) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -22,6 +22,14 @@ ActiveRecord::Schema.define(version: 2021_01_24_233148) do
     t.string "end_time"
     t.integer "total_hours"
     t.string "comments"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "email"
+    t.boolean "admin"
+    t.string "password_digest"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
