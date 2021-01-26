75666047fae3d2fd7c07d964182d6483d71eb988
samhammond87 <sam.hammond1987@gmail.com>
2021-01-26 15:39:47 +1100

    sign up and jwt added

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,9 +3,21 @@ class UsersController < ApplicationController
     def create
         @user = User.create(user_params)
         if @user.save
-            render json: @user, status: :created
+            auth_token = Knock::AuthToken.new payload: {sub: @user.id} # this is a JWT
+            render json: {email: @user.email, jwt: auth_token.token}, status: :created
         else
             render json: @user.errors, status: :unprocessable_entity
+        end
+    end
+
+    def sign_up
+        @user = User.find_by_email(params[email])
+        if @user && @user.authenticate(params[:password])
+            auth_token = Knock::AuthToken.new payload: {sub: @user.id} # this is a JWT
+            render json: {username: @user.email, jwt: auth_token.token}, status: 200
+        else
+            render json: {error: "Incorrect Email or Password"}, status: 404
+            
         end
     end
 
