1917eadd9e636f685eda9c2cfc9461b1925957c6
samhammond87 <sam.hammond1987@gmail.com>
2021-02-07 15:10:33 +1100

    setup cors for netlify 2.0

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'localhost:3000', 'localhost:3001', '127.0.0.1:5050', 'https://dl-constructions.netlify.app/'
+    origins 'localhost:3000', 'localhost:3001', '127.0.0.1:5050', 'https://dl-constructions.netlify.app'
 
     resource '*',
       headers: :any,
